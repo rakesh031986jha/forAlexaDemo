48503eb591c5b02d776d877210759d29987035a0
Rakesh Jha <29140@corp.hexaware.com>
2017-07-05 15:56:39 +0530

    2nd version

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 var Alexa = require("alexa-sdk");
 
 exports.handler = function(event, context, callback) {
+  console.console.log('inside come ');
     var alexa = Alexa.handler(event, context);
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
 
 var handlers = {
+  console.console.log('handle function colling');
     'LaunchRequest': function () {
         this.emit('SayHello');
     },
