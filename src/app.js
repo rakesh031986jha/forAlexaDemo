53eda2de551deac411822c9e70596575210d4298
Rakesh Jha <29140@corp.hexaware.com>
2017-07-05 12:53:34 +0530

    first user

---
+++ src/app.js
@@ -0,0 +1,19 @@
+var Alexa = require("alexa-sdk");
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = {
+    'LaunchRequest': function () {
+        this.emit('SayHello');
+    },
+    'HelloWorldIntent': function () {
+        this.emit('SayHello')
+    },
+    'SayHello': function () {
+        this.emit(':tell', 'Hello World!');
+    }
+};
