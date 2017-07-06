a575526fc716646a4bfaad52b2a67ed066ee8c63
Rakesh Jha <29140@corp.hexaware.com>
2017-07-06 12:38:57 +0530

    new update file

--- src/app.js
+++ src/app.js
@@ -1,15 +1,13 @@
+module.change_code = 1;
 'use strict';
 
 var alexa = require( 'alexa-app' );
 var app = new alexa.app( 'test-skill' );
 
 
-app.launch( function( request, response )
-	response.say( 'Welcome to your test skill' ).
-  reprompt( 'Way to go. You got it to run. Bad ass.' ).shouldEndSession( false );
-}
-console.log('lunch');
- );
+app.launch( function( request, response ) {
+	response.say( 'Welcome to your test skill' ).reprompt( 'Way to go. You got it to run. Bad ass.' ).shouldEndSession( false );
+} );
 
 
 app.error = function( exception, request, response ) {
@@ -23,16 +21,15 @@ app.intent('sayNumber',
   {
     "slots":{"number":"NUMBER"}
 	,"utterances":[
-		"say the number {1-10|number}",
-		"give me the number {1-10|number}",
-		"tell me the number {1-10|number}",
-		"I want to hear you say the number {1-10|number}"]
+		"say the number {1-100|number}",
+		"give me the number {1-100|number}",
+		"tell me the number {1-100|number}",
+		"I want to hear you say the number {1-100|number}"]
   },
   function(request,response) {
     var number = request.slot('number');
     response.say("You asked for the number "+number);
   }
-  console.log('lunch');
 );
 
 module.exports = app;
