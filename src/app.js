65a30f17f8669820f09f14ff447c9ef50cdcd8c2
Rakesh Jha <29140@corp.hexaware.com>
2017-07-06 12:07:49 +0530

    new update file

--- src/app.js
+++ src/app.js
@@ -1,27 +1,35 @@
 'use strict';
 
-var Alexa = require("alexa-sdk");
+var alexa = require( 'alexa-app' );
+var app = new alexa.app( 'test-skill' );
 
-exports.handler = function(event, context, callback) {
-    console.log('inside come ');
 
-    var alexa = Alexa.handler(event, context);
-    alexa.registerHandlers(handlers);
-    alexa.execute();
+app.launch( function( request, response ) {
+	response.say( 'Welcome to your test skill' ).
+  reprompt( 'Way to go. You got it to run. Bad ass.' ).shouldEndSession( false );
+} );
+
+
+app.error = function( exception, request, response ) {
+	console.log(exception)
+	console.log(request);
+	console.log(response);
+	response.say( 'Sorry an error occured ' + error.message);
 };
 
-var handlers = {
-    LaunchRequest: function () {
-        console.log('LaunchRequest');
-        this.emit('SayHello');
-    },
-    HelloWorldIntent: function () {
-        console.log('HelloWorldIntent');
-        this.emit('SayHello')
-    },
-    SayHello: function () {
-        console.log('SayHello');
-        this.emit(':tell', 'Hello World!');
-    }
-};
-console.log('Server running on port');
+app.intent('sayNumber',
+  {
+    "slots":{"number":"NUMBER"}
+	,"utterances":[
+		"say the number {1-100|number}",
+		"give me the number {1-100|number}",
+		"tell me the number {1-100|number}",
+		"I want to hear you say the number {1-100|number}"]
+  },
+  function(request,response) {
+    var number = request.slot('number');
+    response.say("You asked for the number "+number);
+  }
+);
+
+module.exports = app;
